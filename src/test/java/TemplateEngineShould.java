ddbe78ad523f7cecea32cc4d682d8a08dbf53ff4
´josnih´ <josnih@gmail.com>
2023-04-14 11:57:54 +0100

    :sparkles: Initial commit

---
+++ src/test/java/TemplateEngineShould.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TemplateEngineShould {
+    @Test
+    void foo_test(){
+        assertThat(true).isEqualTo(true);
+    }
+}
