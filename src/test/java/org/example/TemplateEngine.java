523ef33a312bba24528bd5f2a29f0115b0cabfae
´josnih´ <josnih@gmail.com>
2023-04-14 13:41:32 +0100

    Refactor: rename variables

--- src/test/java/org/example/TemplateEngine.java
+++ src/test/java/org/example/TemplateEngine.java
@@ -8,15 +8,14 @@ import java.util.regex.Pattern;
 indexOf($)
  */
 public class TemplateEngine {
-    public static String interpolate(String input, Map<String, String> variables) {
-
+    public static String interpolate(String text, Map<String, String> variables) {
         Pattern pattern = Pattern.compile("\\$\\{[a-zA-Z0-9]+\\}");
-        Matcher matcher = pattern.matcher(input);
-        var interpolatedText = input;
+        Matcher matcher = pattern.matcher(text);
+        var interpolatedText = text;
         while (matcher.find()) {
-            String match = matcher.group();
-            String key = match.replace("${", "").replace("}", "");
-            interpolatedText = input.replace(match, variables.get(key));
+            String placeholder = matcher.group();
+            String key = placeholder.replace("${", "").replace("}", "");
+            interpolatedText = text.replace(placeholder, variables.get(key));
         }
         return interpolatedText;
     }
