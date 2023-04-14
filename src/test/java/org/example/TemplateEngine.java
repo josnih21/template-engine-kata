9b8b4e11c1b754a73497e520ea935f4ee098c340
´josnih´ <josnih@gmail.com>
2023-04-14 12:40:18 +0100

    Green: replace_variables_with_values_in_the_dictionary

--- src/test/java/org/example/TemplateEngine.java
+++ src/test/java/org/example/TemplateEngine.java
@@ -1,9 +1,23 @@
 package org.example;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/*
+indexOf($)
+ */
 public class TemplateEngine {
     public static String interpolate(String input, Map<String, String> variables) {
-        return input;
+
+        Pattern pattern = Pattern.compile("\\$\\{[a-zA-Z0-9]+\\}");
+        Matcher matcher = pattern.matcher(input);
+        var interpolatedText = input;
+        while (matcher.find()) {
+            String match = matcher.group();
+            String key = match.replace("${", "").replace("}", "");
+            interpolatedText = input.replace(match, variables.get(key));
+        }
+        return interpolatedText;
     }
 }
