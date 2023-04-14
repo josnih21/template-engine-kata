4ddd0ba13ed116de4ef9fb448645a9a4b1889460
´josnih´ <josnih@gmail.com>
2023-04-14 12:21:10 +0100

    Red: replace_variables_with_values_in_the_dictionary

--- src/test/java/TemplateEngineShould.java
+++ src/test/java/TemplateEngineShould.java
@@ -11,9 +11,17 @@ import static org.assertj.core.api.Assertions.*;
  */
 public class TemplateEngineShould {
     @Test
-    void not_modify_input_if_does_not_contain_variables(){
+    void not_modify_input_if_does_not_contain_variables(){ //TODO: Check name
         String input = "Text";
         var textWithoutInterpolation = TemplateEngine.interpolate(input, Map.of());
         assertThat(textWithoutInterpolation).isEqualTo(input);
+    }
+
+    @Test
+    void replace_variables_with_values_in_the_dictionary(){
+        String input = "Hola amigo me llamo ${name}";
+        var dictionary = Map.of("name", "la Fani");
+        var interpolatedText = TemplateEngine.interpolate(input, dictionary);
+        assertThat(interpolatedText).isEqualTo("Hola amigo me llamo la Fani");
     }
 }
--- src/test/java/org/example/TemplateEngine.java
+++ src/test/java/org/example/TemplateEngine.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Map;
 
 public class TemplateEngine {
-    public static String interpolate(String input, Map<Object, Object> variables) {
+    public static String interpolate(String input, Map<String, String> variables) {
         return input;
     }
 }
